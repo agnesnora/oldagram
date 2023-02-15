83ef719b389605844f0004f2291c0de7605a5f07
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-02-15 11:01:54 +0100

    like function

--- index.js
+++ index.js
@@ -28,12 +28,27 @@ const posts = [
     likes: 152,
   },
 ];
+let feedHtml = document.getElementById("container");
+feedHtml = "";
 
-function stepForward() {
+let post = {};
+render();
+
+function render() {
+  document.getElementById("container").innerHTML = getHTML(posts);
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.likes) {
+    handleLikeClick(e.target.dataset.likes);
+    feedHtml = "";
+    render();
+  }
+});
+
+function getHTML(item) {
   posts.forEach(function (item) {
-    console.log(item);
-
-    document.getElementById("container").innerHTML += `<section>
+    feedHtml += `<section>
     <div class="hero">
       <img src="${item.avatar}" class="profilePic" />
       <div class="user">
@@ -47,14 +62,20 @@ function stepForward() {
     </section>
     <section class="bottom">
     <div class="icon">
-    <img class="likeicon" src="images/like.png">
+    <img class="likeicon" id="${item.likes}" data-likes="${item.username}"src="images/like.png">
     <img class="commenticon" src="images/comment.png">
     <img class="shareicon" src="images/share.png">
     </div>
-    <p class="likeCount"><strong>${item.likes} likes</strong></p>
+    <p id="likes"class="likeCount"><strong>${item.likes} likes</strong></p>
     <p class="comment"><strong>${item.username}</strong> ${item.comment}</p>
     </section>`;
   });
+  return feedHtml;
 }
 
-stepForward();
+function handleLikeClick(username) {
+  const targetPostObj = posts.filter(function (post) {
+    return post.username === username;
+  })[0];
+  targetPostObj.likes++;
+}
