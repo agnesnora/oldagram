c5d0565a73d1ff35a893c51e3083cfbd674b16e1
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-02-15 10:49:13 +0100

    functions

--- index.js
+++ index.js
@@ -28,12 +28,26 @@ const posts = [
     likes: 152,
   },
 ];
+let feedHtml = "";
 
-function stepForward() {
+let post = {};
+render(post);
+
+function render() {
   posts.forEach(function (item) {
-    console.log(item);
+    document.getElementById("container").innerHTML = getHTML(item);
+  });
+}
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.likes) {
+    console.log(e.target.dataset.likes);
+    handleLikeClick(e.target.dataset.likes);
+    render();
+  }
+});
 
-    document.getElementById("container").innerHTML += `<section>
+function getHTML(item) {
+  feedHtml += `<section>
     <div class="hero">
       <img src="${item.avatar}" class="profilePic" />
       <div class="user">
@@ -47,14 +61,41 @@ function stepForward() {
     </section>
     <section class="bottom">
     <div class="icon">
-    <img class="likeicon" src="images/like.png">
+    <img class="likeicon" id="${item.likes}" data-likes="${item.username}"src="images/like.png">
     <img class="commenticon" src="images/comment.png">
     <img class="shareicon" src="images/share.png">
     </div>
-    <p class="likeCount"><strong>${item.likes} likes</strong></p>
+    <p id="likes"class="likeCount"><strong>${item.likes} likes</strong></p>
     <p class="comment"><strong>${item.username}</strong> ${item.comment}</p>
     </section>`;
-  });
+
+  return feedHtml;
 }
 
-stepForward();
+function handleLikeClick(username) {
+  const targetPostObj = posts.filter(function (post) {
+    return post.username === username;
+  })[0];
+  targetPostObj.likes++;
+
+  console.log("targetpostobj.likes:", targetPostObj.likes);
+  console.log("targetpostobj posts:", posts);
+}
+
+// const arrayToObject = (array) =>
+//   array.reduce((obj, item) => {
+//     obj[item.username] = item;
+//     return obj;
+//   }, {});
+// const postsObject = arrayToObject(posts);
+// console.log("postsObject:", postsObject);
+// document.addEventListener("click", function (e) {
+//   console.log("e.t.d.l:", e.target.dataset.likes);
+//   let likesNumber = e.target.dataset.likes;
+
+//   likesNumber++;
+//   console.log(likesNumber);
+//   document.getElementById(
+//     "likes"
+//   ).innerHTML += ` <p id="likes"class="likeCount"><strong>${likesNumber}likes</strong></p>`;
+// });
